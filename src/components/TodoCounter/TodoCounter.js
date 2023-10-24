7b8c3d9183b242ebd55b56c52ffda5c460ac4123
Marco Di Bonaventura <di.bonaventura.flores@gmail.com>
2023-10-24 12:40:15 -0400

    Fix: edit tittle to title

--- src/components/TodoCounter/TodoCounter.js
+++ src/components/TodoCounter/TodoCounter.js
@@ -6,10 +6,10 @@ import './TodoCounter.css';
 function TodoCounter() {
   const { totalTodos: total, completedTodos: completed, loading: isLoading } = React.useContext(TodoContext);
     return (
-      isLoading ?<h2 className="tittle">
+      isLoading ?<h2 className="title">
       loading TODO's...
       </h2> :
-      <h2 className="tittle">
+      <h2 className="title">
         You have completed <span>{completed}</span> of <span>{total}</span> TODO's
       </h2>
     );
