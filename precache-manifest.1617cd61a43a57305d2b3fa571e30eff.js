87d1f005f82243a3b6bc8a27090672deb8de88b2
Marco Di Bonaventura <di.bonaventura.flores@gmail.com>
2023-10-24 14:41:04 -0400

    Updates

---
+++ precache-manifest.1617cd61a43a57305d2b3fa571e30eff.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a3437c39c33f5dd0df01e7637d5cd51",
+    "url": "/react-todo/index.html"
+  },
+  {
+    "revision": "cdcd6cc9a4a230c90042",
+    "url": "/react-todo/static/css/2.e63ffd83.chunk.css"
+  },
+  {
+    "revision": "f97e831a9db7c7acf647",
+    "url": "/react-todo/static/css/main.b0879314.chunk.css"
+  },
+  {
+    "revision": "cdcd6cc9a4a230c90042",
+    "url": "/react-todo/static/js/2.bcda9bd6.chunk.js"
+  },
+  {
+    "revision": "60f6bf9e100e456690e9ab6c9a37bfc2",
+    "url": "/react-todo/static/js/2.bcda9bd6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f97e831a9db7c7acf647",
+    "url": "/react-todo/static/js/main.0cf1af7b.chunk.js"
+  },
+  {
+    "revision": "789a553470789c50a10d",
+    "url": "/react-todo/static/js/runtime-main.b9857b92.js"
+  }
+]);
